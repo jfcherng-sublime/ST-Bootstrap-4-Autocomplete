4bfa1ef6747dcf3d284e5f301761c3f0c1f6e238
Jack Cherng <jfcherng@gmail.com>
2020-01-15 19:43:26 +0800

    Utilize scope: meta.attribute-with-value.class.html
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- bootstrap_4_autocomplete.py
+++ bootstrap_4_autocomplete.py
@@ -6,18 +6,22 @@ import sublime_plugin
 class Bootstrap4Completions(sublime_plugin.EventListener):
     def __init__(self):
         self.class_completions = [("%s \tBootstrap 4 Class" % s, s) for s in bs4_classes]
+        self.working_scopes = ["meta.attribute-with-value.class.html"]
 
     def on_query_completions(self, view, prefix, locations):
-        if view.match_selector(locations[0], "text.html string.quoted"):
+        point = locations[0]
+
+        if view.match_selector(point, "|".join(self.working_scopes)):
+            return self.class_completions
+
+        if view.match_selector(point, "text.html string.quoted"):
             LIMIT = 250
-            cursor = locations[0] - len(prefix) - 1
+            cursor = point - len(prefix) - 1
             start = max(0, cursor - LIMIT - len(prefix))
             line = view.substr(sublime.Region(start, cursor))
             parts = line.split("=")
 
             if len(parts) > 1 and parts[-2].strip().endswith("class"):
                 return self.class_completions
-            else:
-                return []
-        else:
-            return []
+
+        return []
