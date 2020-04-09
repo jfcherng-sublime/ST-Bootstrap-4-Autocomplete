2e06ec20c9442a0f7ebc9366dde2140cc9fb7516
Jack Cherng <jfcherng@gmail.com>
2020-04-09 18:47:12 +0800

    refactor: use JSON data file
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- bootstrap_4_autocomplete.py
+++ bootstrap_4_autocomplete.py
@@ -1,33 +1,20 @@
-from .bootstrap_4_data import classes as bs4_classes
 import sublime
 import sublime_plugin
+
+IS_ST4 = int(sublime.version()) >= 4000
+DATA_FILE = "Packages/%s/data.json" % __package__
 
 
 class Bootstrap4Completions(sublime_plugin.EventListener):
     def __init__(self):
-        if int(sublime.version()) < 4000:
-            self.class_completions = [("%s\tBootstrap 4 Class" % s, s) for s in bs4_classes]
-        else:
-            self.class_completions = list(
-                map(
-                    lambda bs4_class: sublime.CompletionItem(
-                        trigger=bs4_class,
-                        annotation="Bootstrap 4 Class",
-                        completion=bs4_class,
-                        completion_format=sublime.COMPLETION_FORMAT_TEXT,
-                        kind=sublime.KIND_MARKUP,
-                    ),
-                    bs4_classes,
-                )
-            )
-
+        self.completion_items = self.get_completion_items()
         self.working_scopes = ["meta.attribute-with-value.class.html"]
 
-    def on_query_completions(self, view, prefix, locations):
+    def on_query_completions(self, view, prefix, locations) -> list:
         point = locations[0]
 
         if view.match_selector(point, "|".join(self.working_scopes)):
-            return self.class_completions
+            return self.completion_items
 
         if view.match_selector(point, "text.html string.quoted"):
             LIMIT = 250
@@ -37,6 +24,30 @@ class Bootstrap4Completions(sublime_plugin.EventListener):
             parts = line.split("=")
 
             if len(parts) > 1 and parts[-2].strip().endswith("class"):
-                return self.class_completions
+                return self.completion_items
 
         return []
+
+    @staticmethod
+    def get_completion_items() -> list:
+        data = sublime.decode_value(sublime.load_resource(DATA_FILE))
+        annotation = "Bootstrap 4 Class"
+
+        if IS_ST4:
+            completions = list(
+                map(
+                    lambda item: sublime.CompletionItem(
+                        trigger=item[0],
+                        annotation=annotation,
+                        completion=item[0],
+                        completion_format=sublime.COMPLETION_FORMAT_TEXT,
+                        kind=(sublime.KIND_ID_MARKUP, "c", ""),
+                        details="",
+                    ),
+                    data,
+                ),
+            )
+        else:
+            completions = [("%s\t%s" % (item[0], annotation)) for item in data]
+
+        return completions
